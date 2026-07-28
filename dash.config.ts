86518a9e3d86dd69fee3962994f0968de59d1ff2
Yoav <hey@yoav.codes>
2026-07-28 00:25:12 -0400

    hutch-v0.2.0-canary.5

--- dash.config.ts
+++ dash.config.ts
@@ -1,4 +1,4 @@
-// @dash cli=0.2.0-canary.4 cottontail=0.1.1-canary.2
+// @dash cli=0.2.0-canary.5 cottontail=0.1.1-canary.2
 export default {
   scripts: {
     smoke: "examples/smoke.js",
--- src/version.zig
+++ src/version.zig
@@ -1,1 +1,1 @@
-pub const version = "0.2.0-canary.4";
+pub const version = "0.2.0-canary.5";
