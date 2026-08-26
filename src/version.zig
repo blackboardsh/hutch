764def4737a6a601b2fd64a8af1d5776a7af606c
YoavCodes <hey@yoav.codes>
2026-08-26 14:18:22 -0400

    v0.26.0-canary.8

--- src/version.zig
+++ src/version.zig
@@ -1,4 +1,4 @@
-pub const version = "0.26.0-canary.7";
+pub const version = "0.26.0-canary.8";
 
 /// The Cottontail release this Hutch release was built and tested with.
 /// With no pragma and no shim-supplied default, this pair is what runs:
