5fb0218bfbc4022dc2387dc6b0655e16dcff0946
Yoav <hey@yoav.codes>
2026-08-25 22:23:55 -0400

    v0.26.0-canary.5

--- src/version.zig
+++ src/version.zig
@@ -1,4 +1,4 @@
-pub const version = "0.26.0-canary.4";
+pub const version = "0.26.0-canary.5";
 
 /// The Cottontail release this Hutch release was built and tested with.
 /// With no pragma and no shim-supplied default, this pair is what runs:
